bf2b9ba29ffaf8548997ab07cd748875b5453d7e
Core <43536688+kaar1498@users.noreply.github.com>
2021-11-07 11:32:26 +0100

    Create new branch, add old tests

---
+++ src/WebAppTest/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "<Pending>")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "<Pending>")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "<Pending>", Scope = "member", Target = "~F:WebAppTest.IndexTest._component")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>", Scope = "member", Target = "~M:WebAppTest.IndexTest.#ctor")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>", Scope = "member", Target = "~M:WebAppTest.IndexTest.VerifyInitialState")]
---
+++ src/WebAppTest/Pages/IndexTest.cs
@@ -0,0 +1,82 @@
+using AngleSharp.Dom;
+using Bunit;
+using FakeItEasy;
+using System;
+using WebApp.Pages;
+using Xunit;
+
+namespace WebAppTest
+{
+    public class IndexTest : TestContext
+    {
+        private readonly IRenderedComponent<WebApp.Pages.Index> _component;
+        private readonly IElement _input;
+        private readonly IElement _button;
+        private readonly IElement _output;
+
+        public IndexTest()
+        {
+            _component = RenderComponent<WebApp.Pages.Index>();
+            _input = _component.Find("input");
+            _button = _component.Find("button");
+            _output = _component.Find("td");
+
+            VerifyInitialState();
+        }
+
+        private void VerifyInitialState()
+        {
+            _input.MarkupMatches("<input placeholder='xxxx-xxxx-xxxx' class='traceinput'/>");
+            _button.MarkupMatches("<button >Enter</button>");
+            _output.MarkupMatches("<td>Order Recieved</td>");
+        }
+
+        [Theory,
+        InlineData("123456789123", "Order Recieved")]
+        private void Should_returnStatusHistory_When_givenPackageNumber(string first, string expectedResult)
+        {
+            // Arrange
+            _input.Change(first);
+
+            // Act
+            _button.Click();
+
+            string result = _output.TextContent;
+            // Assert
+            Assert.Equal(expectedResult, result);
+
+        }
+
+        [Theory,
+        InlineData("123456789123", "No Order Found")]
+        private void Should_returnInvalid_WhengivenWrongPackageNumber(string first, string expectedResult)
+        {
+            // Arrange
+            _input.Change(first);
+
+            // Act
+            _button.Click();
+
+            string result = _output.TextContent;
+
+            // Assert 
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory,
+        InlineData("Fisk", "Invalid Package Number")]
+        private void Should_returnInvalid_WhengivenInvalidInput(string first, string expectedResult)
+        {
+            // Arrange
+            _input.Change(first);
+
+            // Act
+            _button.Click();
+
+            string result = _output.TextContent;
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
