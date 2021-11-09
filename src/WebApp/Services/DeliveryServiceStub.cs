5bc46b3152fbe9737c30e16bb08ca6c9f49f952f
double-em <meldgaardmike@gmail.com>
2021-11-09 21:32:43 +0100

    Refactor and apply new style

--- src/WebApp/Services/DeliveryServiceStub.cs
+++ src/WebApp/Services/DeliveryServiceStub.cs
@@ -7,27 +7,24 @@ namespace WebApp.Services
 {
     public class DeliveryServiceStub : IDeliveryService
     {
-        private List<Delivery> _deliveries = new List<Delivery>();
-        private List<Status> _statuses = new List<Status>();
-        private Status _deployedStatus;
-        private Status _arrivedStatus;
-        private Delivery _delivery;
+        private readonly List<Status> _statuses = new();
+        private readonly Delivery _delivery;
 
         public DeliveryServiceStub()
         {
-            _deployedStatus = new Status()
+            Status deployedStatus = new()
             {
                 UpdateTime = new DateTime(2021, 10, 29),
                 Message = "Package has been deployed"
             };
             
-            _arrivedStatus = new Status()
+            Status arrivedStatus = new()
             {
                 UpdateTime = DateTime.Now,
                 Message = "Package has arrived at destination"
             };
-            _statuses.Add(_deployedStatus);
-            _statuses.Add(_arrivedStatus);
+            _statuses.Add(deployedStatus);
+            _statuses.Add(arrivedStatus);
             _delivery = new Delivery()
             {
                 TracingId = "asdasd2233",
